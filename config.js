3fdaec90888bd469afb380586f46fa8391d1bee1
Luis <louiscrack50@gmail.com>
2022-01-10 17:06:59 -0600

    agregando database en la nube

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    // database: {
-    //     username: "unyrwh08yqr804xg",
-    //     password: "lc6QcgoJdYFEctT00M6C",
-    //     database: "bwgipodhmmasmnq158nz",
-    //     host: "bwgipodhmmasmnq158nz-mysql.services.clever-cloud.com",
-    //     port: 3306
-    // }
     database: {
-        username: "root",
-        password: "root",
-        database: "adminecommerce",
-        host: "localhost"
+        username: "unyrwh08yqr804xg",
+        password: "lc6QcgoJdYFEctT00M6C",
+        database: "bwgipodhmmasmnq158nz",
+        host: "bwgipodhmmasmnq158nz-mysql.services.clever-cloud.com",
+        port: 3306
     }
+    // database: {
+    //     username: "root",
+    //     password: "root",
+    //     database: "adminecommerce",
+    //     host: "localhost"
+    // }
 }

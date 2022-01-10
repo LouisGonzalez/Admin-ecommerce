d9c91a27cfa1dffa374c13027a7f73c32f0ae807
Luis <louiscrack50@gmail.com>
2022-01-10 12:04:01 -0600

    password de la db

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 module.exports = {
     database: {
-        username: "root",
-        password: "root",
-        database: "adminecommerce",
-        host: "localhost"
+        username: "unyrwh08yqr804xg",
+        password: "lc6QcgoJdYFEctT00M6C",
+        database: "bwgipodhmmasmnq158nz",
+        host: "bwgipodhmmasmnq158nz-mysql.services.clever-cloud.com",
+        port: 3306
     }
 }
